7b5a73d393ce6e902b8eec690cbc69073f8e14d3
Jamal Sebbari <sebbarija@gmail.com>
2022-08-29 16:37:47 +0200

    change moment js by day js

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './styles/App.css';
 import Enfant from './Enfant';
-import moment from 'moment';
-import 'moment/locale/fr'
+import dayjs from 'dayjs'
+import 'dayjs/locale/fr'
+
+
+
 
 function App() {
-  let date = moment().format('LLLL');
-  
+  dayjs.locale('fr')
+  let date = dayjs().format('D MMMM YYYY')
 
 
   const article= "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui asperiores perferendis numquam voluptatem quos perspiciatis at a similique accusamus, delectus harum, quaerat quisquam repellendus odio minus reiciendis ad quas optio expedita dolorem minima vero? Minima voluptatem inventore rerum perspiciatis nostrum placeat pariatur facilis, enim nemo, illo nesciunt nihil tempora voluptate!"
